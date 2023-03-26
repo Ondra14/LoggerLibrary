3f8e8e6a89b408f165ebb89716af30fbb407bab8
Ondřej Veselý <ondrej.vesely@sequenex.com>
2023-03-26 10:48:48 +0200

    Fix documentation.

--- Sources/LoggerLibrary/LoggerLevel.swift
+++ Sources/LoggerLibrary/LoggerLevel.swift
@@ -5,20 +5,31 @@
 //  Created by Ondřej Veselý on 20.01.2023.
 //
 
+/// An enumeration representing the various levels of logging supported by a logger instance.
+///
+/// Each case of the enumeration corresponds to a specific logging level, with `disabled` representing the lowest level of logging and `error` representing the highest level.
+///
+/// - Note: This enumeration conforms to the `Int`, `CaseIterable`, and `Codable` protocols, allowing it to be used in a variety of contexts.
 public enum LoggerLevel: Int, CaseIterable, Codable {
-    /// Disables logging.
+    /// The `disabled` logging level, indicating that no logging should be performed.
     case disabled
-    /// log something generally unimportant (lowest priority)
+    
+    /// The `verbose` logging level, providing the most detailed logging output.
     case verbose
-    /// log something which help during debugging (low priority)
+    
+    /// The `debug` logging level, providing detailed logging output for debugging purposes.
     case debug
-    /// log something which you are really interested but which is not an issue or error (normal priority)
+    
+    /// The `info` logging level, providing general information about the application's state.
     case info
-    /// log something which may cause big trouble soon (high priority)
+    
+    /// The `warning` logging level, indicating potential issues or areas of concern.
     case warning
-    /// log something which will keep you awake at night (highest priority)
+    
+    /// The `error` logging level, indicating a critical error that requires attention.
     case error
-
+    
+    /// The default logging level, used when a specific level is not specified.
     public static let defaultLevel = LoggerLevel.warning
 }
 
